6f39df19b9bf56e980928561b94aaa16d4515f88
RonEerdekensPXL <ron.eerdekens@student.pxl.be>
2024-03-05 11:38:29 +0100

    Code coverage demo6b edit exception on  actor

--- src/main/java/de/syngenio/demo6b/Actor.java
+++ src/main/java/de/syngenio/demo6b/Actor.java
@@ -1,6 +1,8 @@
 package de.syngenio.demo6b;
 
 public class Actor {
-	public void moveMotor(int amount) {};
+	public void moveMotor(int amount){
+		throw new UnsupportedOperationException("notImplemented() empty methode");
+	};
 	public void stopMotor() {};
 }
