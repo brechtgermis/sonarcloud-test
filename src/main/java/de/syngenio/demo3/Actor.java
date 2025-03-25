cdde3320ed137194d7fc3677464adff869884fa0
JelleOeyenPXL <jelle.oeyen@student.pxl.be>
2025-03-25 11:50:27 +0100

    fixed

--- src/main/java/de/syngenio/demo3/Actor.java
+++ src/main/java/de/syngenio/demo3/Actor.java
@@ -1,6 +1,20 @@
 package de.syngenio.demo3;
 
 public class Actor {
-	public void moveMotor(int amount) {};
-	public void stopMotor() {};
+	public void moveMotor(int amount) {
+		/*
+		 * This method should move the motor by the given amount.
+		 * The motor should be moved by the given amount.
+		 * If the amount is positive, the motor should be moved in the direction of heating.
+		 * If the amount is negative, the motor should be moved in the direction of cooling.
+		 * The motor should be stopped if the amount is 0.
+		*/
+		throw new UnsupportedOperationException("Not yet implemented");
+	};
+	public void stopMotor() {
+		/*
+		 * This method should stop the motor.
+		*/
+		throw new UnsupportedOperationException("Not yet implemented");
+	};
 }
